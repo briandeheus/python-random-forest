fa2a073df62f13d8dd5faf4efd58e615ad01b412
Brian de Heus <me@brian.jp>
2020-03-06 18:24:05 +0800

    Adds an additional test to test randomness

--- test.py
+++ test.py
@@ -8,6 +8,12 @@ class TestRandomForest(unittest.TestCase):
         rf = random_forest.RandomForest()
         self.assertIsNotNone(rf.get())
 
+    def test_randomness(self):
+        rf = random_forest.RandomForest()
+        forests = set([rf.get() for _ in range(0, 100)])
+        # Out of a hundred random forests, we should at least get more than one forest.
+        self.assertTrue(len(forests) > 1)
+
 
 if __name__ == '__main__':
     unittest.main()
